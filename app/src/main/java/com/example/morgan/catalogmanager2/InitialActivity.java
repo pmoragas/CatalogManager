f296671a5af4756224ccd81e8c44e96e60d3c33d
pmoragas <pmoragas@gmail.com>
2018-03-16 18:32:31 +0100

    commit prova

---
+++ app/src/main/java/com/example/morgan/catalogmanager2/InitialActivity.java
@@ -0,0 +1,22 @@
+package com.example.morgan.catalogmanager2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class InitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+    }
+
+}
